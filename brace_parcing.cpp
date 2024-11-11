9c116d00efffd161309f8730722ac92838450c51
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-11-11 20:53:44 +0300

    reading the file

--- brace_parcing.cpp
+++ brace_parcing.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "Binary_Tree.cpp"
+#include "stack.cpp"
 
 int main()
 {
     std::string input;
-    getline(std::cin, input);
+    Stack<char> stack;
+    std::ifstream f("brace_input.txt");
+    if (f)
+        getline(f, input);
     if (input[0] != '(')
     {
         std::cout << "Error!" << std::endl;
         return 1;
     }
+    stack.push_front('(');
     input.erase(0, 1);
     int root = 0;
     if (input.find('(') > 5)
@@ -21,6 +27,7 @@ int main()
         root = root*10 + input[i]-'0';
     input.erase(0, input.find('('));
     Binary_Tree<int> Tree(root);
+    int curr_node_val = 0;
     for (int i = 0; i < input.size(); i++)
     {
         
---
+++ stack.cpp
@@ -0,0 +1,36 @@
+template<typename S>
+struct Node_solo
+{
+    S value;
+    Node_solo* next;
+    Node_solo<S>(Node_solo<S>* _next, S _value) {next = _next; value = _value;}
+};
+
+template<typename S>
+class Stack
+{
+    Node_solo<S>* head;
+public:
+    Stack() {head = nullptr;}
+    void push_front(S);
+    void pop_front();
+    S get_value() {return head->value;}
+    bool is_empty() {if (head == nullptr) return true; return false;}
+private:
+    Node_solo<S>* front() {return head;}
+};
+
+template<typename S>
+void Stack<S>::push_front(S _val)
+{
+    Node_solo<S>* curr = new Node_solo<S>(head, _val);
+    head = curr;
+}
+
+template<typename S>
+void Stack<S>::pop_front()
+{
+    Node_solo<S>* curr = head;
+    head = head->next;
+    delete curr;
+}
