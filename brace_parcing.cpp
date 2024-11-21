ffd2dd5576b7089155611b5dcb8cb9d26e394926
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-11-21 21:51:02 +0300

    menu imp

--- brace_parcing.cpp
+++ brace_parcing.cpp
@@ -3,6 +3,17 @@
 #include "Binary_Tree.cpp"
 #include "Red_Black_Tree.cpp"
 
+void menu()
+{
+    std::cout << "1. Удалить узел" << std::endl;
+    std::cout << "2. Добавить узел" << std::endl;
+    std::cout << "3. Проверить существует ли узел" << std::endl;
+    std::cout << "4. Обход дерева в ширину" << std::endl;
+    std::cout << "5. Обход дерева в глубину" << std::endl;
+    std::cout << "6. Выход" << std::endl;
+    std::cout << "> ";
+}
+
 int main()
 {
     std::string input;
@@ -97,15 +108,90 @@ int main()
     }
     Vector<int> RB_input(1);
     RB_Tree<int> RB_Tree;
+    std::cout << "Центрированный обход бинарного дерева в глубину" << std::endl;
     Binary_Tree.deep_traverse(Binary_Tree.get_root(), RB_input);
     std::cout << std::endl;
     for (int i = 0; i < RB_input.get_size(); i++)
         RB_Tree.add(RB_input[i]);
-    RB_Tree.depth_first_search_forward();
-    std::cout << std::endl;
-    RB_Tree.depth_first_search_central();
-    std::cout << std::endl;
-    RB_Tree.depth_first_search_backward();
-    std::cout << std::endl; 
+    std::cout << "Дерево успешно построено" << std::endl;
+    char c = '\0';
+    while (c != '6')
+    {
+        menu();
+        std::cin >> c;
+        std::cout << std::endl;
+        switch(c)
+        {
+            case '1':
+            {
+                std::cout << "Какой узел вы хотите удалить? ";
+                int to_del;
+                std::cin >> to_del;
+                RB_Tree.delete_node(to_del);
+                system("clear");
+                break;
+            }
+
+            case '2':
+            {
+                std::cout << "Какой узел вы хотите добавить? ";
+                int to_add;
+                std::cin >> to_add;
+                RB_Tree.add(to_add);
+                system("clear");
+                break;
+            }
+                
+            case '3':
+            {
+                std::cout << "Какой узел вы хотите найти? ";
+                int to_find;
+                std::cin >> to_find;
+                RB_Tree_node<int>* curr = RB_Tree.search(nullptr, to_find);
+                system("clear");
+                if (curr->key == to_find)
+                    std::cout << "Узел " << to_find << " есть в дереве" << std::endl;
+                else
+                    std::cout << "Узла " << to_find << " нет в дереве" << std::endl;
+                break;
+            }
+                
+            case '4':
+            {
+                RB_Tree.breadth_first_search();
+                std::cin.ignore();
+                std::cout << std::endl << "Нажмите Enter для продолжения ";
+                std::cin.ignore();
+                system("clear");
+                break;
+            }
+
+            case '5':
+            {
+                char dfs_c = '\0';
+                std::cout << "1. Прямой обход" << std::endl;
+                std::cout << "2. Центрированный обход" << std::endl;
+                std::cout << "3. Обратный обход" << std::endl;
+                std::cout << "> ";
+                std::cin >> dfs_c;
+                if (dfs_c == '1')
+                    RB_Tree.depth_first_search_forward();
+                if (dfs_c == '2')
+                    RB_Tree.depth_first_search_central();
+                if (dfs_c == '3')
+                    RB_Tree.depth_first_search_backward();
+                std::cin.ignore();
+                std::cout << std::endl << "Нажмите Enter для продолжения ";
+                std::cin.ignore();
+                system("clear");
+                break;
+            }
+                
+            case '6':
+                break;
+            default:
+                break;
+        }
+    }
     return 0;
 }
